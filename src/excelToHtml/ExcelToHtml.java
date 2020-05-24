1327b140392148e9d28f2b65d2c16ec33d7f9e32
houjie <houjie82@hotmail.com>
2020-05-24 18:31:25 +0800

    excel2html

---
+++ src/excelToHtml/ExcelToHtml.java
@@ -0,0 +1,20 @@
+package excelToHtml;
+
+import java.io.File;
+
+public class ExcelToHtml {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		File file=new File("d:/2019104»ÆìÏº¼.xls");
+		try {
+			String str=ExcelTransformHtml.getExcelInfo(file);
+			System.out.println(str);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
